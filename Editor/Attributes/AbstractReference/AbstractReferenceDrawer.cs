8a31ae9c6b6acd981a0be4407f374ba6df4795cb
Erik Kubiak <erikkubiak@gmail.com>
2022-03-18 17:20:20 +0100

    Changes

--- Editor/Attributes/AbstractReference/AbstractReferenceDrawer.cs
+++ Editor/Attributes/AbstractReference/AbstractReferenceDrawer.cs
@@ -1,6 +1,7 @@
 ﻿using EditorUtilities.Editor.Attributes.AbstractReference.PropertyHandler;
 using EditorUtilities.Editor.Extensions;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace EditorUtilities.Editor.Attributes.AbstractReference
@@ -8,27 +9,42 @@ namespace EditorUtilities.Editor.Attributes.AbstractReference
     [CustomPropertyDrawer(typeof(AbstractReferenceAttribute))]
     public class AbstractReferenceDrawer : PropertyDrawer
     {
+        private ArrayPropertyHandler m_ArrayProperty;
+
+        private ArrayPropertyHandler ArrayProperty => m_ArrayProperty ??= new ArrayPropertyHandler();
+        
+        private DirectPropertyHandler m_DirectPropertyHandler;
+
+        private DirectPropertyHandler DirectPropertyHandler => m_DirectPropertyHandler ??= new DirectPropertyHandler();
+        
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            EditorGUI.BeginChangeCheck();
             APropertyHandler handler = GetRightPropertyHandler(property);
+            handler.Init(property);
             handler.HandleProperty(position, property, label);
+            if (EditorGUI.EndChangeCheck())
+            {
+                property.serializedObject.ApplyModifiedProperties();
+            }
         }
 
         private APropertyHandler GetRightPropertyHandler(SerializedProperty property)
         {
+            
             if (fieldInfo.FieldType.IsArray)
             {
-                return new ArrayPropertyHandler(property);
+                return ArrayProperty;
             }
 
-            return new DirectPropertyHandler(property);
+            return DirectPropertyHandler;
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            return property.GetInstanceField().Value == null
+            return APropertyHandler.Padding * 2 + (property.GetInstanceField().Value == null
                 ? EditorGUIUtility.singleLineHeight
-                : EditorGUI.GetPropertyHeight(property);
+                : EditorGUI.GetPropertyHeight(property)) + APropertyHandler.PostDropdownMargin(property);
         }
     }
 }
